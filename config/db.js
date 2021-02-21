aadcc780528fa3808c2be147896ff54409638aa7
Ashish Jha <ashishat763@gmail.com>
2021-02-22 01:00:47 +0530

    Profile changes

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const db = config.get('mongoURI');
 const connectDB = async () => {
     try
     {
-      await  mongoose.connect(db, {useNewUrlParser: true, useCreateIndex: true});
+      await  mongoose.connect(db, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false});
 
       console.log("Mongo DB connected")
     }
